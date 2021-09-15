bd7da2066ba5b74aa7896936f1108cd94b312be8
TheNimum <70508714+TheNimum@users.noreply.github.com>
2021-09-15 10:08:08 +0200

    steg åt rätt rikting

--- src/components/C-Body.vue
+++ src/components/C-Body.vue
@@ -2,8 +2,12 @@
 <div class="main-Container">
 	<h2> Main </h2>
 	<input id="temp-name" v-model="search" placeholder="Sök" >
-	<button id="tryck här" v-on:click="sendRequest">tryck här</button>
-	<div><p>name: {{apiReturn.name}}</p></div>
+	<button id="tryck" v-on:click="sendRequest">tryck här</button>
+	<div>
+		<ul id="exempel1">
+		<li>
+		{{ apiReturn}}
+		</li></ul></div>
 
 </div>
 
@@ -13,17 +17,21 @@
 export default ({
 	data: () => ( {
 		search: '0',
-		apiReturn: {} 	
+		apiReturn: null
+		
+		
+			
 	}),
 	methods: {
 		async sendRequest() {
 			const url = `https://swapi.dev/api/people/?search=${this.search}`
 			try {
-				const resP = await fetch(url)
-				const data = await resP.json()
+				const response = await fetch(url)
+				const data = await response.json()				
+				console.log('api:', data);
+				
 				this.apiReturn = data;
-				console.log('data from api:', data);
-			}
+				}
 			catch{
 				return null;
 			}
