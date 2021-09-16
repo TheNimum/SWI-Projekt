bac6b69f790b298c588bc33d7a2e2c45d268fc39
TheNimum <70508714+TheNimum@users.noreply.github.com>
2021-09-16 13:42:13 +0200

    Start

--- src/components/C-Body.vue
+++ src/components/C-Body.vue
@@ -2,15 +2,17 @@
 <div class="main-Container">
 	<h2> Main </h2>
 	<input id="temp-name" v-model="search" placeholder="Sök" >
-	<button id="tryck" v-on:click="sendRequest">tryck här</button>
+	<button class="tryck" v-on:click="sendRequest">tryck här</button>
+	<button class="getall" v-on:click="getRequest">hämta lista</button>
 	<div>
-		<ul id="exempel1">
+		<ul class="temp-list">
 		<li> name: {{apiReturn.name}}</li>
 		<li> birth year: {{apiReturn.birth_year}}</li>
 		<li> lenght: {{apiReturn.height}}</li>
-		
 		</ul></div>
-
+	<div class="listOfChar">
+		{{characters}}
+	</div>
 </div>
 
 </template>
@@ -19,29 +21,44 @@
 export default ({
 	data: () => ( {
 		search: '0',		
-			apiReturn: { },
-			searchQuery: null
+		apiReturn: {},
+		characters: {},
+		
+			
 	}),
 	
 	
 	methods: {
 		async sendRequest() {
 			const url = `https://swapi.dev/api/people/?search=${this.search}`
-			try {
-				
+			try {				
 				const response = await fetch(url)
 				const data = await response.json()				
 				console.log('api:', data);
-
-				this.apiReturn = data.results[0];
-				
+				this.apiReturn = data.results[0];				
 			}
 			catch{
 				return null;
 			}
+		},
+		async getRequest()
+		{
+			const url = `https://swapi.dev/api/people/?search=${this.search}`
+			
+			const response = await fetch(url)
+			const data = await response.json()
+			
+			for (let i = 0; i < data.length; i++) {
+				
+				const x = data.results[i];
+				this.characters = x;
+			}
+			console.log('data', data)
+				
+			
+			
 		}
-
-	}
+	} 
 })
 </script>
 
@@ -49,5 +66,12 @@ export default ({
 .main-Container{
 	background-color: black;
 	border: dotted rgb(129, 199, 221);
+}
+.temp-list{
+	border: dotted hotpink;
+	transition: 0,5s;
+	margin-left: 15em;
+	margin-right: 15em;
+	align-content: center;
 }
 </style>
--- src/components/C-Footer.vue
+++ src/components/C-Footer.vue
@@ -3,7 +3,7 @@
 	<h3>beskrivning</h3>
 	
 	<div class="test">
-	<p> målet är att appen ska kunna visa lisa av 
+	<p> målet är att appen ska kunna visa lista av 
 		olika saker inom StarWars miljön. ex karaktärer eller planeter etc </p>
 	</div>
 
@@ -21,5 +21,7 @@ export default ({
 </script>
 
 <style scoped>
-
+.test{
+	border: dotted yellow;
+}
 </style>
