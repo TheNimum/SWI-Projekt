fcf5177c616d7e993e46309930e6635b5b0ccbac
TheNimum <mikael.pajari@live.se>
2021-09-20 14:26:21 +0200

    tagit bort nexttick onödig

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,7 +3,7 @@
 <div class="headerLogo">
   <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Star_Wars_Logo.svg/1280px-Star_Wars_Logo.svg.png">
 </div>
-></header>
+</header>
 </template>
 
 <script>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -42,9 +42,9 @@ export default ({
 		peopleinfo: false,
 	}),
 
-	mounted(){this.$nextTick(function(){
+	mounted(){
 		this.getRequest();
-	})},
+	},
 
 
 	methods: {
